0ba35e3eaf65f7c5054bc142626ccb14c3c3f86c
RESHMA REGHU <47914@user>
2017-06-23 11:42:38 +0530

    msg

--- in.c
+++ in.c
@@ -4,17 +4,19 @@ void read();
 int n,array[10];
 main(){
   int i;
+  read(array);
   sort(array);
 
-  for(i=0;i<7;i++)
+  for(i=0;i<n;i++)
     {
       printf("%d  ",array[i]);
     }
+  printf("\n");
   return 0;
 }
 void sort(int *array)
 {int c,d,t,i;
- for (c = 1 ; c <=6; c++) {
+ for (c = 1 ; c <=n-1; c++) {
     d = c;
  
     while ( d > 0 && array[d] < array[d-1]) {
@@ -27,7 +29,7 @@ void sort(int *array)
   }
 }
 void read()
-{
+{int i;
   printf("enter size\n");
   scanf("%d",&n);
   printf("enter items");
