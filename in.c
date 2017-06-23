75a3e99664c54bf208582e843064d2848ff42c05
RESHMA REGHU <47914@user>
2017-06-23 10:50:20 +0530

    sort complete

---
+++ in.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+  int array[7]={4,23,43,38,15,10,30};
+  int c,d,t,i;
+ for (c = 1 ; c <=6; c++) {
+    d = c;
+ 
+    while ( d > 0 && array[d] < array[d-1]) {
+      t          = array[d];
+      array[d]   = array[d-1];
+      array[d-1] = t;
+ 
+      d--;
+    }
+  }
+
+  for(i=0;i<7;i++)
+    {
+      printf("%d  ",array[i]);
+    }
+  return 0;
+}
