37bcc9c138ee4216fc02ae5fef0fac1c5a4515ce
RESHMA REGHU <47914@user>
2017-06-23 10:59:28 +0530

    success

--- in.c
+++ in.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
+void sort(int *array);
 main(){
   int array[7]={4,23,43,38,15,10,30};
   int c,d,t,i;
+  sort(array);
+
+  for(i=0;i<7;i++)
+    {
+      printf("%d  ",array[i]);
+    }
+  return 0;
+}
+void sort(int *array)
+{int c,d,t,i;
  for (c = 1 ; c <=6; c++) {
     d = c;
  
@@ -13,10 +24,4 @@ main(){
       d--;
     }
   }
-
-  for(i=0;i<7;i++)
-    {
-      printf("%d  ",array[i]);
-    }
-  return 0;
 }
